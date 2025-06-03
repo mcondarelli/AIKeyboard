70502f0252e6b9149ec8bdfd97e92d7ea77af420
Mauro Condarelli <mc5686@mclink.it>
2025-06-03 22:41:51 +0200

    Avoid calling platform-specific functions when running on other platforms.

--- src/aikeyboard/input_injection/input_manager.py
+++ src/aikeyboard/input_injection/input_manager.py
@@ -1,19 +1,19 @@
 # src/aikeyboard/keyboard/input_manager.py
 import platform
 
-from aikeyboard.input_injection.linux import LinuxInput
-from aikeyboard.input_injection.macos import MacInput
-from aikeyboard.input_injection.windows import WindowsInput
 
 
 class InputManager:
     def __init__(self):
         system = platform.system()
         if system == "Windows":
+            from aikeyboard.input_injection.windows import WindowsInput
             self.impl = WindowsInput()
         elif system == "Linux":
+            from aikeyboard.input_injection.linux import LinuxInput
             self.impl = LinuxInput()
         elif system == "Darwin":
+            from aikeyboard.input_injection.macos import MacInput
             self.impl = MacInput()
         else:
             raise OSError(f"Unsupported system: {system}")
